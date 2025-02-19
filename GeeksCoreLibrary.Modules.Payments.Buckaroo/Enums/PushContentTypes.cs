bad870452d75c7e482585beb47dbcab9e717ee29
Marco Ketelaars <marcoketelaars@happyhorizon.com>
2025-02-19 14:39:25 +0100

    Add support for HTTP GET push handling in BuckarooService

--- GeeksCoreLibrary.Modules.Payments.Buckaroo/Enums/PushContentTypes.cs
+++ GeeksCoreLibrary.Modules.Payments.Buckaroo/Enums/PushContentTypes.cs
@@ -13,5 +13,10 @@ public enum PushContentTypes
     /// <summary>
     /// The push request is comprised of form data.
     /// </summary>
-    HttpPost = 2
+    HttpPost = 2,
+    
+    /// <summary>
+    /// The push request is a get request with query data
+    /// </summary>
+    HttpGet = 3
 }
